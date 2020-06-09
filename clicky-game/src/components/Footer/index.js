71f9f6dfe9ee4bc9def86dc290e5ab938a65046c
Ragnorok303 <jasontompkins22@gmail.com>
2020-06-09 07:53:49 -0600

    modified files and image

---
+++ clicky-game/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="bottom">
+                <img alt="react" src="assets/images/soccerball.png" />
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
