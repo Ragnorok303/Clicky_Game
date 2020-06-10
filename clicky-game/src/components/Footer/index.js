79162201338d2beaa5c5a8762414f19b8eb6b30e
Ragnorok303 <jasontompkins22@gmail.com>
2020-06-10 11:07:38 -0600

    react-create-app on Heroku

---
+++ clicky-game/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="bottom">
+             React Clicky Game<img alt="react" src="assets/images/soccerball.png" />
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
